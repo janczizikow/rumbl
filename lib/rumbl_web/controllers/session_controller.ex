825c1064fe37d513373229ff119f41b299e00df4
Jan Czizikow <jan@gohidoc.com>
2020-07-26 22:33:48 +0200

    authentication plug 🔌

---
+++ lib/rumbl_web/controllers/session_controller.ex
@@ -0,0 +1,30 @@
+defmodule RumblWeb.SessionController do
+  use RumblWeb, :controller
+
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def create(
+    conn,
+    %{"session" => %{"username" => username, "password" => password}}
+  ) do
+    case Rumbl.Accounts.authenticate_by_username_and_password(username, password) do
+      {:ok, user} ->
+        conn
+        |> RumblWeb.Auth.login(user)
+        |> put_flash(:info, "Welcome back!")
+        |> redirect(to: Routes.page_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Invalid username or password")
+        |> render("new.html")
+    end
+  end
+
+  def delete(conn, _params) do
+    conn
+    |> RumblWeb.Auth.logout()
+    |> redirect(to: Routes.page_path(conn, :index))
+  end
+end
